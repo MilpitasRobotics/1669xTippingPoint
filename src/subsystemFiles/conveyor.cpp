c72d993e561eb350e7e1224675cd5ec07f6976d5
Kathan Sheth <sheth.kathan04@gmail.com>
2021-12-22 20:10:14 -0800

    modified conveyor.cpp

--- src/subsystemFiles/conveyor.cpp
+++ src/subsystemFiles/conveyor.cpp
@@ -1,18 +1,18 @@
 #include "main.h"
 
-int reverse = 1;
+int reverse = 1
 bool conveyor_lock = false;
 bool conveyor_up = false;
 void move_conveyor(){
   if (controller.get_digital(DIGITAL_R2)) {
-    reverse = -1;
+    reverse = -1
     conveyor_up = !conveyor_up;
     conveyor_lock = true;
+    while(controller.get_digital(DIGITAL_R2)){sleep(0.01)}
   }
   else if (!controller.get_digital(DIGITAL_R2) && conveyor_lock==true) {
-    reverse = 1;
+    reverse = 1
     conveyor_lock = false;
-    conveyor_up = !conveyor_up;
   }
 
   if (conveyor_up)
